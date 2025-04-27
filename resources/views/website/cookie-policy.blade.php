d51b700e6fe66092b645f70428bc35d1f352e14b
Mert Bağ <bagmert1996@gmail.com>
2025-04-27 20:44:42 +0300

    feat: prepare legal pages

--- resources/views/website/cookie-policy.blade.php
+++ resources/views/website/cookie-policy.blade.php
@@ -4,9 +4,69 @@
 
 @section('content')
     <!-- Cookie Policy Section -->
-    <section class="container mx-auto min-h-[calc(100dvh-449px)] flex flex-col justify-center items-center">
-        <h1 class="text-7xl text-primary text-center">
+    <section class="container mx-auto py-12 w-1/2 flex flex-col space-y-6">
+
+        <h1 class="font-semibold text-2xl text-primary">
             Cookie Policy
         </h1>
+
+        <p>
+            Laoreet suspendisse interdum consectetur libero id faucibus nisl tincidunt eget. 
+            Donec ultrices tincidunt arcu non. Accumsan sit amet nulla facilisi morbi. 
+            Pellentesque pulvinar pellentesque habitant morbi tristique senectus et.
+        </p>
+
+        <p>
+            Scelerisque fermentum dui faucibus in ornare quam viverra. Fermentum posuere urna nec tincidunt praesent.
+            Turpis cursus in hac habitasse platea dictumst quisque. Ut faucibus pulvinar elementum integer enim
+            neque volutpat ac. Hendrerit gravida rutrum quisque non tellus orci. Est sit amet facilisis magna.
+            Aliquet risus feugiat in ante metus dictum at tempor. Viverra accumsan in nisl nisi. Blandit massa
+            enim nec dui nunc mattis.
+        </p>
+
+        <p>
+            Dignissim suspendisse in est ante. Commodo odio aenean sed adipiscing. Vulputate eu scelerisque felis
+            imperdiet proin fermentum. Neque aliquam vestibulum morbi blandit. Lectus arcu bibendum at varius vel
+            pharetra vel turpis. Pellentesque pulvinar pellentesque habitant morbi tristique senectus et.
+            Adipiscing enim eu turpis egestas pretium aenean pharetra. Accumsan sit amet nulla facilisi morbi.
+            Enim nunc faucibus a pellentesque sit amet porttitor eget dolor.
+        </p>
+
+        <p>
+            Id interdum velit laoreet id donec ultrices. Viverra justo nec ultrices dui sapien eget mi proin sed.
+            Vitae et leo duis ut diam.
+        </p>
+
+        <p>
+            Commodo ullamcorper a lacus vestibulum sed arcu. Neque aliquam vestibulum morbi blandit cursus risus at
+            ultrices. Dignissim suspendisse in est ante. Commodo odio aenean sed adipiscing. Vulputate eu scelerisque
+            felis imperdiet proin fermentum. Neque aliquam vestibulum morbi blandit.
+        </p>
+
+        <p>
+            Tempus quam pellentesque nec nam aliquam sem et tortor. Cursus mattis molestie a iaculis at erat
+            pellentesque adipiscing commodo. Amet justo donec enim diam vulputate ut pharetra sit. Scelerisque
+            viverra mauris in aliquam sem fringilla. Commodo ullamcorper a lacus vestibulum sed arcu. Viverra
+            accumsan in nisl nisi. Blandit massa enim nec dui nunc mattis.
+        </p>
+
+        <p>
+            Commodo odio aenean sed adipiscing. Vulputate eu scelerisque felis imperdiet proin fermentum.
+            Neque aliquam vestibulum morbi blandit.
+        </p>
+
+        <p>
+            Lectus arcu bibendum at varius vel pharetra vel turpis. Pellentesque pulvinar pellentesque habitant
+            morbi tristique senectus et. Adipiscing enim eu turpis egestas pretium aenean pharetra. Dolor purus
+            non enim praesent elementum facilisis leo vel. Bibendum neque egestas congue quisque egestas diam in.
+            Elit duis tristique sollicitudin nibh sit amet commodo nulla.
+        </p>
+
+        <p>
+            Neque aliquam vestibulum morbi blandit. Vulputate eu scelerisque felis imperdiet proin fermentum.
+            Donec ultrices tincidunt arcu non. Accumsan sit amet nulla facilisi morbi. Id interdum velit laoreet
+            id donec ultrices. Viverra justo nec ultrices dui sapien eget mi proin sed. Vitae et leo duis ut diam.
+            Cursus mattis molestie a iaculis at erat pellentesque adipiscing commodo. Amet justo pharetra sit.
+        </p>
     </section>
 @endsection
